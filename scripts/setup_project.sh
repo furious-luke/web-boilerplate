05adc2dd3cfec7eebb953a2598249bc3b1e51ca3
Luke Hodkinson <furious-luke@gmail.com>
2016-06-11 09:23:52 +1000

    * Don't try to rename directories if already done.  * Remove Heroku Toolbelt installation.

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-mv _project_ $1
-mv $1/_project_ $1/$1
+if [ -d _project_ ]; then
+    mv _project_ $1
+fi
+if [ -d $1/_project_ ]; then
+    mv $1/_project_ $1/$1
+fi
 find . -type f -not -name setup_project.sh | xargs grep -l _project_ | xargs -n 1 sed -i "" "s/_project_/$1/g"
