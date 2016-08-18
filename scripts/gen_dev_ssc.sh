ca454a2e1876f86fcbcaa7da09d1b7b8e7f4f1da
Luke Hodkinson <furious.luke@gmail.com>
2016-08-18 14:09:58 +1000

    Have the channels setup working in production mode.

--- scripts/gen_dev_ssc.sh
+++ scripts/gen_dev_ssc.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 openssl req -x509 -newkey rsa:2048 -keyout keys/dev.key -out keys/dev.cert -nodes -subj '/CN=localhost'
+cat keys/dev.key keys/dev.cert > keys/dev.pem
--- scripts/web.sh
+++ scripts/web.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # gunicorn --workers 1 $WSGI_APP
-daphne $ASGI_APP
+daphne $ASGI_APP -b 0.0.0.0 -p $PORT
