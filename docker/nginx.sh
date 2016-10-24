8d5b7f79a10aad61ad703fd25fd619a211aa8ab4
Luke Hodkinson <furious.luke@gmail.com>
2016-10-24 22:02:32 +1100

    Can now use dynamic upstream hosts on nginx.

--- docker/nginx.sh
+++ docker/nginx.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# Generate our upstream string.
+WEB_PROCESSES=3
+UPSTREAM=
+for ii in $(seq 0 $(($WEB_PROCESSES - 1))); do
+    x=$(printf "80%02d" $ii)
+    UPSTREAM="$UPSTREAM server 0.0.0.0:$x;"
+done
+export UPSTREAM
+
 envsubst '$WORKER_PROCESSES $WORKER_CONNECTIONS $SERVER_NAME $UPSTREAM $PORT' < /etc/nginx/templ/nginx.conf > /etc/nginx/nginx.conf && exec nginx -g 'daemon off;'
