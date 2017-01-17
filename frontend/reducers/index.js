f09d2cb87c8317eec6d4bc84a57dea178e24f1e9
Luke Hodkinson <furious.luke@gmail.com>
2017-01-18 09:56:48 +1100

    Minor updates.

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -1,13 +1,15 @@
-import { combineReducers } from 'redux';
-import { routerReducer as routing } from 'react-router-redux';
-import { reducer as form } from 'redux-form';
+import {combineReducers} from 'redux';
+import {routerReducer as routing} from 'react-router-redux';
+import {reducer as form} from 'redux-form';
+import {reducer as model} from 'redux-jam';
 
-import { authReducer as auth } from '../boilerplate/reducers';
+import {authReducer as auth} from '../boilerplate/reducers';
 
 const rootReducer = combineReducers({
   auth,
   routing,
-  form
+  form,
+  model
 });
 
 export default rootReducer;
